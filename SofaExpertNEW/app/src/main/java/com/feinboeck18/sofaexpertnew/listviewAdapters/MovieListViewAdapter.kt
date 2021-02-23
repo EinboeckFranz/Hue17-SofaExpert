257942e81fa7272e4a0e990bb3254cf27a4ee949
BuildTools <unconfigured@null.spigotmc.org>
2021-02-23 09:24:52 +0100

    changed some Stuff -> Readability

--- SofaExpertNEW/app/src/main/java/com/feinboeck18/sofaexpertnew/listviewAdapters/MovieListViewAdapter.kt
+++ SofaExpertNEW/app/src/main/java/com/feinboeck18/sofaexpertnew/listviewAdapters/MovieListViewAdapter.kt
@@ -1,5 +1,6 @@
 package com.feinboeck18.sofaexpertnew.listviewAdapters
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.view.View
 import android.view.ViewGroup
@@ -9,15 +10,16 @@ import com.feinboeck18.sofaexpertnew.Movie
 import com.feinboeck18.sofaexpertnew.R
 
 class MovieListViewAdapter(private val context: Activity, var movies: List<Movie>) : ArrayAdapter<Movie>(context, R.layout.imageview_movie, movies) {
+    @SuppressLint("InflateParams")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val currentView = convertView ?: context.layoutInflater.inflate(R.layout.imageview_movie,null,true)
 
-        Movie.loadImg(currentView.findViewById(R.id.moviePoster), MainActivity.BASIC_IMG_URL + (getItem(position)?.posterURLPath))
+        Movie.loadImg(currentView.findViewById(R.id.moviePoster), MainActivity.BASIC_IMG_URL + (getItem(position).posterURLPath))
 
         return currentView
     }
 
-    override fun getItem(position: Int): Movie? {
+    override fun getItem(position: Int): Movie {
         return movies[position]
     }
 }
